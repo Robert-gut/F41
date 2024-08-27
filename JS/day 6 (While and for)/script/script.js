8748813040405cff0d5b952bbf8277addfbead0c
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2024-08-27 21:16:58 +0300

    for and while2

--- JS/day 6 (While and for)/script/script.js
+++ JS/day 6 (While and for)/script/script.js
@@ -194,3 +194,15 @@ const namberGame = () => {
 }
 
 namberGame()
+
+
+
+const sum = (n) => {
+  let sum = 0
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) sum += i
+  }
+  return sum
+}
+
+sum(50)
